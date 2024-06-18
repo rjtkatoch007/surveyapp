6e471406faf326806d574df8594a678a8dad73e9
rajat <rajat.k@sphinxworldbiz.co>
2024-06-18 19:31:41 +0530

    Diffrent templates created e.g DefaultLayout and GuestLayout and other

--- reactvel/src/router.jsx
+++ reactvel/src/router.jsx
@@ -1,27 +1,45 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Surveys from "./views/Surveys";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/" />
+            },
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/surveys',
+                element: <Surveys />
+            },
+        ]
     },
     {
-        path: '/surveys',
-        element: <Surveys />
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/signup',
-        element: <Signup />
-    },
+        path: '/guest',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                path: 'signup',
+                element: <Signup />
+            },
+        ]
+    }
+
 ])
 
 export default router;
--- reactvel/src/views/Surveys.jsx
+++ reactvel/src/views/Surveys.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 function Surveys() {
     return (
-        <div>
-            <h6>Surveys</h6>
-        </div>
+        <>
+            <header className="bg-white shadow">
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
+                </div>
+            </header>
+            <main>
+                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">Surveys Content</div>
+            </main>
+        </>
     )
 }
 
