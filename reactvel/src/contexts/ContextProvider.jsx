586242b603b7884fb625fba78fbf084c6c5b5592
rajat <rajat.k@sphinxworldbiz.co>
2024-06-20 18:40:20 +0530

    Reuseable componet PageComponent created with ContextProvider

---
+++ reactvel/src/contexts/ContextProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext({});
+
+export const ContextProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState({});
+    const [userToken, setUserToken] = useState(null);
+
+    return (
+        <StateContext.Provider value={{
+            currentUser,
+            setCurrentUser,
+            userToken,
+            setUserToken,
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+
+}
+
+export const useStateContext = () => useContext(StateContext);
